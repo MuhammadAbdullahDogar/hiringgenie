b03b44cfec02818e531bb5cc1c9b2bd4c8ffdaff
Muhammad Abdullah <abdullah@pop-os.localdomain>
2022-10-04 18:49:06 +0500

    Updates

---
+++ precache-manifest.3925b46016c9eed09b120cdc2eeb63cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "56eb192739140ef27418",
+    "url": "/hiringgenie/static/css/main.d9524338.chunk.css"
+  },
+  {
+    "revision": "56eb192739140ef27418",
+    "url": "/hiringgenie/static/js/main.56eb1927.chunk.js"
+  },
+  {
+    "revision": "8f2f4211b0891106f089",
+    "url": "/hiringgenie/static/js/1.8f2f4211.chunk.js"
+  },
+  {
+    "revision": "dafa51ee842b3e62f657",
+    "url": "/hiringgenie/static/js/2.dafa51ee.chunk.js"
+  },
+  {
+    "revision": "0b4bf65742052da8e7ab",
+    "url": "/hiringgenie/static/js/runtime~main.0b4bf657.js"
+  },
+  {
+    "revision": "620bec9f15b319be029e7b0851f28937",
+    "url": "/hiringgenie/index.html"
+  }
+];
