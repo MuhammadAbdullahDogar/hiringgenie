60f703bc35a2199daeae517f561d4e873a72d977
Muhammad Abdullah <abdullah@pop-os.localdomain>
2022-10-11 23:33:06 +0500

    Updates

---
+++ precache-manifest.fea0279a3646af33252c2f9661731389.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "dc1581655ca3e4eac589",
+    "url": "/hiringgenie/static/css/main.f755febd.chunk.css"
+  },
+  {
+    "revision": "dc1581655ca3e4eac589",
+    "url": "/hiringgenie/static/js/main.dc158165.chunk.js"
+  },
+  {
+    "revision": "f0ff088a22a065b3a207",
+    "url": "/hiringgenie/static/js/1.f0ff088a.chunk.js"
+  },
+  {
+    "revision": "000591256221b53ca91c",
+    "url": "/hiringgenie/static/js/2.00059125.chunk.js"
+  },
+  {
+    "revision": "5f9f91c1b9c0fc5768cb",
+    "url": "/hiringgenie/static/js/runtime~main.5f9f91c1.js"
+  },
+  {
+    "revision": "f9eabbcc4ee80d07e7c31028aaec336a",
+    "url": "/hiringgenie/static/media/logo.f9eabbcc.svg"
+  },
+  {
+    "revision": "dda555dcc860ae0df44635a618359e90",
+    "url": "/hiringgenie/static/media/men_img.dda555dc.svg"
+  },
+  {
+    "revision": "759614a00ede330e254d95b8eb17ac4e",
+    "url": "/hiringgenie/static/media/women_img.759614a0.svg"
+  },
+  {
+    "revision": "b32a0d2ddebac87710c690e8e8bcfdc8",
+    "url": "/hiringgenie/static/media/bg_des.b32a0d2d.svg"
+  },
+  {
+    "revision": "a84d920aadd1fbc28733b7904ef39361",
+    "url": "/hiringgenie/static/media/home_center_element.a84d920a.svg"
+  },
+  {
+    "revision": "91e0f7583d335c897cbbc8a5837ba4d6",
+    "url": "/hiringgenie/static/media/section_design.91e0f758.svg"
+  },
+  {
+    "revision": "982922d2e64a5ec924b16c63bc0e9de1",
+    "url": "/hiringgenie/static/media/curve_line.982922d2.svg"
+  },
+  {
+    "revision": "8b549467a788692f298527e469fcb6e1",
+    "url": "/hiringgenie/static/media/login_line.8b549467.svg"
+  },
+  {
+    "revision": "5dde8ecab992c9c17495842620ec38c8",
+    "url": "/hiringgenie/static/media/signup_line.5dde8eca.svg"
+  },
+  {
+    "revision": "1e73a72cb267df02e5f3d251ecb7cd67",
+    "url": "/hiringgenie/index.html"
+  }
+];
