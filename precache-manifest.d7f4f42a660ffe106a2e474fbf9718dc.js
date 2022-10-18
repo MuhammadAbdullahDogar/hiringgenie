e663ec461ad8c15ea72b15fe29aa7c867cc19386
Muhammad Abdullah <abdullah@pop-os.localdomain>
2022-10-18 21:52:11 +0500

    Updates

---
+++ precache-manifest.d7f4f42a660ffe106a2e474fbf9718dc.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "2ed20e93b0f68e7bc998",
+    "url": "/hiringgenie/static/css/main.f786f0ec.chunk.css"
+  },
+  {
+    "revision": "2ed20e93b0f68e7bc998",
+    "url": "/hiringgenie/static/js/main.2ed20e93.chunk.js"
+  },
+  {
+    "revision": "2b01ef10c9deb1aeeeb8",
+    "url": "/hiringgenie/static/js/1.2b01ef10.chunk.js"
+  },
+  {
+    "revision": "528e16579cef6fedae5a",
+    "url": "/hiringgenie/static/js/2.528e1657.chunk.js"
+  },
+  {
+    "revision": "3ebab4689bbe0cd747ce",
+    "url": "/hiringgenie/static/js/runtime~main.3ebab468.js"
+  },
+  {
+    "revision": "dda555dcc860ae0df44635a618359e90",
+    "url": "/hiringgenie/static/media/men_img.dda555dc.svg"
+  },
+  {
+    "revision": "759614a00ede330e254d95b8eb17ac4e",
+    "url": "/hiringgenie/static/media/women_img.759614a0.svg"
+  },
+  {
+    "revision": "f9eabbcc4ee80d07e7c31028aaec336a",
+    "url": "/hiringgenie/static/media/logo.f9eabbcc.svg"
+  },
+  {
+    "revision": "5dde8ecab992c9c17495842620ec38c8",
+    "url": "/hiringgenie/static/media/signup_line.5dde8eca.svg"
+  },
+  {
+    "revision": "3f2608ff31270ab6aa7c49348377ae7e",
+    "url": "/hiringgenie/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/hiringgenie/precache-manifest.bcabf3f66254a89e5f383a5e1dec1c61.js"
+  "/hiringgenie/precache-manifest.d7f4f42a660ffe106a2e474fbf9718dc.js"
 );
 
 workbox.clientsClaim();
