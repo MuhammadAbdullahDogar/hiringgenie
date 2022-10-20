b394daf012251184ee3c01e8e380b4bc65369a94
Muhammad Abdullah <abdullah@pop-os.localdomain>
2022-10-20 21:11:56 +0500

    Updates

---
+++ precache-manifest.809755fb5a2fce887ac150d8f7c00964.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "cc045cda7d9ca1bccdea",
+    "url": "/home/static/css/main.a29bfa14.chunk.css"
+  },
+  {
+    "revision": "cc045cda7d9ca1bccdea",
+    "url": "/home/static/js/main.cc045cda.chunk.js"
+  },
+  {
+    "revision": "33c6ba1fe6f19a8d8c8b",
+    "url": "/home/static/js/1.33c6ba1f.chunk.js"
+  },
+  {
+    "revision": "2973eae2145612e587e6",
+    "url": "/home/static/js/2.2973eae2.chunk.js"
+  },
+  {
+    "revision": "e4895a2f0f11730ee7f7",
+    "url": "/home/static/js/runtime~main.e4895a2f.js"
+  },
+  {
+    "revision": "dda555dcc860ae0df44635a618359e90",
+    "url": "/home/static/media/men_img.dda555dc.svg"
+  },
+  {
+    "revision": "759614a00ede330e254d95b8eb17ac4e",
+    "url": "/home/static/media/women_img.759614a0.svg"
+  },
+  {
+    "revision": "f9eabbcc4ee80d07e7c31028aaec336a",
+    "url": "/home/static/media/logo.f9eabbcc.svg"
+  },
+  {
+    "revision": "5dde8ecab992c9c17495842620ec38c8",
+    "url": "/home/static/media/signup_line.5dde8eca.svg"
+  },
+  {
+    "revision": "de2b7af07ef2d0ce4bbc4722d48ec1c7",
+    "url": "/home/index.html"
+  }
+];
